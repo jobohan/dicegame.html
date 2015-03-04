bcfa7fbda8284d98a9681a9b27dc73a3b789f620
jobohan <jobohan@s.sfusd.edu>
2015-03-03 17:51:27 -0800

    h
    h

--- dice.js
+++ dice.js
@@ -53,7 +53,7 @@ $('.die.selectable').attr('data-roll', randomRoll);//mine
 function holdDie() {
     // TODO: toggle the class 'hold' for the die that was just clicked
     // HINT: It will start with $(this)
-$(die).toggleClass(hold);
+$(this).toggleClass('hold');
     $('.die').addClass('selectable');//mine
     // Leave this here to update the state after the die is held
     $('.game').trigger('update-game-state');
@@ -61,7 +61,7 @@ $(die).toggleClass(hold);
 
 function restartGame() {
     // EXAMPLE:
-    $('die').addClass('selectable')// TODO: add the class 'selectable' to every element
+    $('.die').addClass('selectable');// TODO: add the class 'selectable' to every element
     //       with the 'die' class
     
     // EXAMPLE:
